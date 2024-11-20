4d84fa6f740e824accefa4d94016daed70952628
IsharaDayasena <141900612+IsharaDayasena@users.noreply.github.com>
2024-11-20 23:10:24 +0530

    SignIn/UpPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -11,18 +13,20 @@ const App = () => {
       {/* Overall Layout */}
       <div className="flex flex-col min-h-screen">
         {/* Header */}
-        <Header />
+        
 
         {/* Main Content */}
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </main>
 
         {/* Footer */}
-        <Footer />
+        
       </div>
     </Router>
   );
