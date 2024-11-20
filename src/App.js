785e6773292a04c945507486b57aeec7f19282e1
oshini <samarasingheoshini2001@gmail.com>
2024-11-21 00:08:48 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
+import CartPage from "./pages/Cart";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/cart" element={<CartPage />} />
           </Routes>
         </main>
 
