061027af78382be06b9dbdeb4b6ff366175f17ad
Neranji Abeysekara <155614380+NeranjiAbeysekara@users.noreply.github.com>
2024-11-21 10:56:37 +0530

    header
    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
-
+import Navbar from "./components/Navbar/Navbar";
 const App = () => {
   return (
     <Router>
       {/* Overall Layout */}
       <div className="flex flex-col min-h-screen">
         {/* Header */}
-        <Header />
+        <Navbar/>
 
         {/* Main Content */}
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact-us" element={<ContactUs />} />
+            
           </Routes>
         </main>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Home = () => {
-  return <div>hfjffj</div>;
+  return <div>
+    hfjffj
+    </div>;
 };
 
 export default Home;
